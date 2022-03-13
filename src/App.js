018ede026d67b46707c2fcb19f3700416582f6ee
iradbouzidi <irad.bouzidi@espit.com>
2022-03-13 17:01:11 +0100

    added CommerceJS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
 
 import { Navbar, Products, Cart, Checkout } from "./components";
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Products />
-  </div>
-);
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(cart);
+  return (
+    <div>
+      <Navbar />
+      <Products products={products} onAddToCart={handleAddToCart} />
+    </div>
+  );
+};
 
 export default App;
+// 59:00
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,33 +2,18 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Product from "./Product/Product";
+import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "shoes",
-    description: "running shoes",
-    price: "$150",
-    image:
-      "https://assets.adidas.com/images/w_600,f_auto,q_auto/1b4917151ab348868e4aad7a00921b60_9366/Superstar_Shoes_White_GW0570_01_standard.jpg",
-  },
-  {
-    id: 2,
-    name: "PC",
-    description: "Dell PC",
-    price: "$1500",
-    image:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-silver-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1633027804000",
-  },
-];
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
 
-const Products = () => {
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
